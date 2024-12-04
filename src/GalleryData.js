42731c694fb0c8a6f868004a427cb4a7818a8e54
Khanh <Khanh13Nguyen13@gmail.com>
2024-12-04 19:12:20 +0100

    create gallery

---
+++ src/GalleryData.js
@@ -0,0 +1,42 @@
+export const GalleryData = [
+	{
+		id: 1,
+		title: "cafe-01",
+		src: "./img/cafe-01.jpg",
+	},
+	{
+		id: 2,
+		title: "cafe-02",
+		src: "./img/cafe-02.jpg",
+	},
+	{
+		id: 3,
+		title: "cafe-03",
+		src: "./img/cafe-03.jpg",
+	},
+	{
+		id: 4,
+		title: "cafe-04",
+		src: "./img/cafe-04.jpg",
+	},
+	{
+		id: 5,
+		title: "cafe-05",
+		src: "./img/cafe-05.jpg",
+	},
+	{
+		id: 6,
+		title: "cafe-06",
+		src: "./img/cafe-06.jpg",
+	},
+	{
+		id: 7,
+		title: "cafe-07",
+		src: "./img/cafe-07.jpg",
+	},
+	{
+		id: 8,
+		title: "cafe-08",
+		src: "./img/cafe-08.jpg",
+	},
+];
